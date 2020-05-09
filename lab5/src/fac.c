a718a5eea01198383b81dc8ded2866a01179c066
antonzuzz <antonzuzz@yandex.ru>
2020-05-09 14:00:08 +0000

    lab5: 2 ex complited

--- lab5/src/fac.c
+++ lab5/src/fac.c
@@ -17,14 +17,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-void do_fac(int *);
-void do_wrap_up(int);
-int result = 1;
-int *r;
+struct for_fac_struc {
+    int begin;
+    int end;
+    int mod;
+    int *result;
+};
+
+//void do_fac(int *);
+//void do_wrap_up(int);
+int factmod (int n, int p);
+void result_fac(struct for_fac_struc *struc);
+void start_thread(int beg, int end, int mod, int *result, int j);
+
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+    pthread_t threads[100];
+    struct for_fac_struc struc[100];
 
 int main(int argc, char **argv) {
-    pthread_t *threads;
+    int result = 1;
     int k=-1, pnum=-1, mod=-1;
 
 while (1) {
@@ -52,7 +63,7 @@ while (1) {
                 break;
             case 1:      
                 pnum = atoi(optarg);
-                if (pnum <= 0) {
+                if (pnum <= 0 || pnum > 100) {
                     printf("pnum is a positive number\n");
                     return 1;
                 }
@@ -71,9 +82,9 @@ while (1) {
         default:
         printf("!d\n");
             printf("getopt returned character code 0%o?\n", c);
-    }
-      
+    } 
   }
+
   if (optind < argc) {
     printf("Has at least one no option argument\n");
     return 1;
@@ -84,36 +95,83 @@ while (1) {
            argv[0], k, pnum, mod);
     return 1;
   }
-   printf("%d\n", mod);
-  
 
+    int step = k / pnum;
+    int beg = 1;
+    int end = 1 + step + k % pnum;
 
-/*
-  if (pthread_create(&thread1, NULL, (void *)do_one_thing,
-			  (void *)&common) != 0) {
-    perror("pthread_create");
-    exit(1);
-  }
+    printf("step - %d\n", step);
+    int j;
 
-  if (pthread_create(&thread2, NULL, (void *)do_another_thing,
-                     (void *)&common) != 0) {
-    perror("pthread_create");
-    exit(1);
-  }
+    for (j = 0; j < pnum; j++) {
 
-  if (pthread_join(thread1, NULL) != 0) {
-    perror("pthread_join");
-    exit(1);
-  }
+       
+        start_thread(beg, end, mod, &result, j);
+       
+        beg = end;
+        end += step;
+    }
 
-  if (pthread_join(thread2, NULL) != 0) {
-    perror("pthread_join");
-    exit(1);
-  }
+    for (j = 0; j < pnum; j++) {
+        if (pthread_join(threads[j], NULL) != 0) {
+            perror("pthread_join");
+            exit(1);
+        }
+    }
 
-  do_wrap_up(common);
-*/
+  printf("result - %d\n", result);
   return 0;
+}
+
+void start_thread(int beg, int end, int mod, int *result, int j) {
+
+        struc[j].begin = beg;
+        struc[j].end = end;
+        struc[j].mod = mod;
+        struc[j].result = result;
+
+        printf("%d - %d\n", struc[j].begin, struc[j].end);
+
+        if (pthread_create(&threads[j], NULL, (void *)result_fac,
+                    &struc[j]) != 0) {
+            perror("pthread_create");
+            exit(1);
+        }
+}
+
+
+void result_fac(struct for_fac_struc *struc) 
+{
+    
+    int begin=struc->begin;
+    int end=struc->end;
+    int mod=struc->mod;
+    int i;
+    int res = 1;
+    int buf;
+
+    for (i = begin; i < end; i++) {
+        buf = i % mod;
+        res *= i;
+        res %= mod;
+    }
+
+    pthread_mutex_lock(&mut);
+    *(struc->result) *= res;
+    *(struc->result) %= mod;
+    pthread_mutex_unlock(&mut);
+}
+
+int factmod (int n, int p) {
+	int res = 1;
+	while (n > 1) {
+		res = (res * ((n/p) % 2 ? p-1 : 1)) % p;
+        int i=2;
+		for (; i <= n%p; ++i)
+			res = (res * i) % p;
+		n /= p;
+	}
+	return res % p;
 }
 
 /*
